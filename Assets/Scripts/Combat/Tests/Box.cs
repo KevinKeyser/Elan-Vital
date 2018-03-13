b53804a361f420b3e56e85b2f373d883f0129ca2
Kevin Keyser <kevin.keyser@live.com>
2018-03-13 04:51:04 -0700

    Test For Checking IWeapon Deals Damage To IHealth

---
+++ Assets/Scripts/Combat/Tests/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElanVital.Combat;
+using UnityEngine;
+
+namespace ElanVital.Combat
+{
+    public class Box : MonoBehaviour, IHealth
+    {
+        public bool IsDead => health <= 0;
+
+        [SerializeField]
+        private int health;
+        public int Health => health;
+
+        public void ModifyHealth(int value)
+        {
+            health = Mathf.Max(0, health + value);
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ElanVital.Combat
+{
+    public class Weapon : MonoBehaviour, IWeapon
+    {
+        [SerializeField]
+        private bool requiresBothHands = false;
+        public bool RequiresBothHands => requiresBothHands;
+
+        [SerializeField]
+        [Range(1, short.MaxValue)]
+        private int damage = 1;
+        public int Damage => damage;
+
+        [SerializeField]
+        private Collider[] damageColliders = new Collider[0];
+
+        public void DealDamage(IHealth health)
+        {
+            health.ModifyHealth(-damage);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject)
+            {
+                var health = collision.gameObject.GetComponent<IHealth>();
+                if (health != null)
+                {
+                    DealDamage(health);
+                }
+            }
+        }
+    }
+}
