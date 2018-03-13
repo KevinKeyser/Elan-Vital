2877938d83419335bf51b965b6122bc0a69221e4
Robert Sandor <robert.i.sandor@gmail.com>
2018-03-13 05:54:09 -0700

    Have initial version of inverse-kinematics

---
+++ Assets/Scripts/DynamicAnimation/TestScene/Bone.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bone : MonoBehaviour {
+
+    public float Length;
+
+    public Transform Target;
+
+    private List<Bone> childBones;
+
+    public List<Bone> ChildBones
+    {
+        get
+        {
+            if(childBones == null)
+            {
+                childBones = new List<Bone>();
+                for (int index = 0; index < transform.childCount; index++)
+                {
+                    Bone childBone = transform.GetChild(index).GetComponent<Bone>();
+                    childBone.Target = Target;
+                    if(childBone != null)
+                    {
+                        childBones.Add(childBone);
+                        childBones.AddRange(childBone.childBones);
+                        break;
+                    }
+                }
+            }
+
+            return childBones;    
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        if(ChildBones.Count > 0)
+        {
+            Length = Vector3.Distance(transform.position, ChildBones[0].transform.position);
+        }
+        else
+        {
+            Length = 0;
+        }
+	}
+}
