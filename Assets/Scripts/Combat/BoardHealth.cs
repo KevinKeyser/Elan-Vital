813c67b55cef0b6201f0c11ca1433d4c107ea58b
Robert Sandor <robert.i.sandor@gmail.com>
2018-03-14 13:57:45 -0700

    Added board with health

---
+++ Assets/Scripts/Combat/BoardHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ElanVital.Combat;
+
+namespace ElanVital.Combat
+{
+    public class BoardHealth : MonoBehaviour, IHealth
+    {
+        private bool isDead;
+        public bool IsDead
+        {
+            get
+            {
+                return isDead;
+            }
+        }
+        private int health;
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+        }
+        public void ModifyHealth(int value)
+        {
+            health = Mathf.Max(0, health + value);
+            if(health <= 0)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
