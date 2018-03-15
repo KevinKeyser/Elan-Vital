ff43f733eb4a71a21ad83a7b0441599374262695
Ryan Scopio <ryanscopio@gmail.com>
2018-03-14 17:31:34 -0700

    added monster movement

---
+++ Assets/Scripts/Travel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Travel : MonoBehaviour
+{
+    public Transform TravelPoints;
+    public float Speed;
+    private List<Transform> points;
+    private int target = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        points = new List<Transform>();
+        foreach (Transform child in TravelPoints.transform)
+        {
+            points.Add(child);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = Speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, points[target].position, step);
+
+        if (Vector3.Distance(transform.position, points[target].position) < 1)
+        {
+            target++;
+            if (target >= points.Count) target = 0;
+        }
+    }
+}
